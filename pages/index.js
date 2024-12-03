2949c3aecb2d968ae5b1011f2e6060f5d2c7936c
thiagoGna <87281809+thiagoGna@users.noreply.github.com>
2024-12-03 08:25:22 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-function Home(){
-
-    return <h1>For yourself, keep going...<p>Keep coding...</p></h1>
-
+function Home() {
+  return (
+    <h1>
+      For yourself, keep going...<p>Keep coding...</p>
+    </h1>
+  );
 }
 
 export default Home;
